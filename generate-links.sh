47cd1f8ce7d40c3d7663e08eb08649ad71d19cab
Ishan Darji <ishan616@yahoo.com>
2025-05-22 16:07:47 -0400

    started adding shell scripts

---
+++ generate-links.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+ln -sf ~/linux-dotfiles/.bashrc ~/
+ln -sf ~/linux-dotfiles/.bash_profile ~/
+ln -sf ~/linux-dotfiles/.profile ~/
+ln -sf ~/linux-dotfiles/.icons ~/
+
+ln -sf ~/linux-dotfiles/ghostty ~/.config
+ln -sf ~/linux-dotfiles/hypr ~/.config
+ln -sf ~/linux-dotfiles/nvim ~/.config
+ln -sf ~/linux-dotfiles/oh-my-posh ~/.config
+ln -sf ~/linux-dotfiles/waybar ~/.config
+ln -sf ~/linux-dotfiles/wofi ~/.config
+ln -sf ~/linux-dotfiles/yazi ~/.config
