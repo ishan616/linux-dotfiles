d38b8142e1f9d4ed50de2211898163da8608b5d3
Ishan Darji <ishan616@yahoo.com>
2025-06-20 14:49:20 -0400

    auto install oh-my-posh

--- install-deps.sh
+++ install-deps.sh
@@ -1,3 +1,10 @@
 #! /usr/bin/bash
 
 sudo pacman -S $(cat dependancies)
+
+#install oh-my-posh
+cd /tmp/
+git clone https://aur.archlinux.org/oh-my-posh.git
+cd oh-my-posh/
+makpkg -sirc --noconfirm
+cd
