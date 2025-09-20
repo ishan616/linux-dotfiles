0cdc97f9786e31ee1e47dd3659c32d18256fba6a
Ishan Darji <ishan616@yahoo.com>
2025-09-20 01:02:35 -0400

    tweaks

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,7 @@ require("config.lazy")
 require("venv-selector").setup({
   search = {
     my_custom_venvs = {
-      command = "fd '/bin/python$' ~/.python_venvs -iH --full-path", -- Searches for 'python' files in a specific directory
+      command = "fd '/bin/python$' ~/.virtualenvs -iH --full-path", -- Searches for 'python' files in a specific directory
     },
   },
 })
