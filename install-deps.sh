bf3d283d2ea4540b6ae9d09d712ef93d6cbd992f
Ishan Darji <ishan616@yahoo.com>
2025-09-20 00:40:10 -0400

    remove wlogout

--- install-deps.sh
+++ install-deps.sh
@@ -5,10 +5,10 @@ sudo pacman -S $(cat dependancies) --noconfirm
 #install oh-my-posh
 cd /tmp/
 git clone https://aur.archlinux.org/oh-my-posh.git
-git clone https://aur.archlinux.org/wlogout.git
+#git clone https://aur.archlinux.org/wlogout.git
 cd oh-my-posh/
 makepkg -sirc --noconfirm
-cd ../wlogout/
-gpg --keyserver pgp.mit.edu --recv-keys E25D679AF73C6D2F
-makepkg -sirc --noconfirm
+#cd ../wlogout/
+#gpg --keyserver pgp.mit.edu --recv-keys E25D679AF73C6D2F
+#makepkg -sirc --noconfirm
 cd
